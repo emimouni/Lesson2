a68c746f60e6c0a53064e4ac4869d73de64e5bc3
Elias  Mimouni <em343@pvhs-lab112-d07.pvsd.org>
2016-09-08 10:33:28 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s; //Declaring
+        s = "Holla!"; // Initializing
+        String another = "Hey Homie"; // All on the same line
+        System.out.println("Holla!");
+        String s2 = "";
+        //later on...
+        s2="Elias";
+
+        int age = 89;
+        // can't just call the variable whatever I want
+        //I have to play by some rules
+        //1. No names that are reserved
+        //such as: for, if, do, while, system, else...
+        //2. same named variables are named
+        //3. 22- can't use numbers, but I can't start with a nymber, a2 for example is allowed
+        //4. No symbols
+        //5. No Spaces: First name doesn't work, but First_Name or firstName does
+    }
+}
